40945e01835fb98c02d47336f6768438189f2f5c
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-08-24 07:12:37 +0100

    Add hovers on the button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -17,7 +17,7 @@ function Calculator() {
     <div className="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
       <button
         type="button"
-        className={`rounded-full h-12 w-20 flex items-center ${color} justify-center shadow-lg hover:border-gray-500 focus:outline-none ${style}`}
+        className={`hover:bg-sky-700 rounded-full h-12 w-20 flex items-center ${color} justify-center shadow-lg hover:border-gray-500 focus:outline-none ${style}`}
         name={name}
         onClick={() => handleClick(name)}
       >
